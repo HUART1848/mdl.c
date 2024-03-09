8e1165b6bab483a5dc9c2b5aeb50b432fb64b561
Hugo Huart <hugo.huart@heig-vd.ch>
2024-03-09 19:05:53 +0100

    image: use binary ppm format

--- mdl.c
+++ mdl.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 #define W 2000
 #define H 2000
 #define MAXITER 128
 
-unsigned char img[W*H];
+unsigned char imgdata[W*H*3];
 
 unsigned char mdlc(float x, float y) {
     float zr = 0.0;
@@ -14,13 +15,11 @@ unsigned char mdlc(float x, float y) {
     float ci = y;
 
     for (int i = 0; i < MAXITER; ++i) {
-        float nzr = zr * zr - zi * zi;
-        float nzi = (2 * zr * zi);
+        float nzr = zr * zr - zi * zi + cr;
+        float nzi = (2 * zr * zi) + ci;
+
         zr = nzr;
         zi = nzi;
-
-        zr += cr;
-        zi += ci;
         if (zr * zr + zi * zi > 4.0) {
             return (unsigned char) ((255.0 * i) / MAXITER);
         }
@@ -28,16 +27,17 @@ unsigned char mdlc(float x, float y) {
     return 0;
 }
 
-void imgtoppm(char *filename, unsigned char *img, int w, int h) {
+void imgtoppm(char *filename, unsigned char *imgdata, int w, int h) {
+    /* Write header */
     FILE *f = fopen(filename, "w");
-    fprintf(f, "P3\n");
+    fprintf(f, "P6\n");
     fprintf(f, "%d %d\n", w, h);
     fprintf(f, "255\n");
+    fclose(f);
 
-    for (int i = 0; i < w * h; ++i) {
-        fprintf(f, "%d %d %d\n", img[i], img[i], img[i]);
-    }
-
+    /* Write pixels */
+    f = fopen(filename, "ab");
+    fwrite(imgdata, 1, w * h * 3, f);
     fclose(f);
 }
 
@@ -45,15 +45,22 @@ int main(void) {
     float x1 = -1.5, x2 = 1.5;
     float y1 = -1.5, y2 = 1.5;
 
+    puts("Computing...");
     for (int i = 0; i < H; ++i) {
         for (int j = 0; j < W; ++j) {
             float x = x1 + (x2 - x1) * ((float) j / W);
             float y = y1 + (y2 - y1) * ((float) i / H);
 
-            img[j + i * W] = mdlc(x, y);
+            unsigned char c = mdlc(x, y);
+            int idx = (j + i * W) * 3;
+            imgdata[idx] = c;
+            imgdata[idx+1] = c;
+            imgdata[idx+2] = c;
         }
     }
 
-    imgtoppm("mdl.ppm", img, W, H);
+    puts("Writing to file...");
+    imgtoppm("mdl.ppm", imgdata, W, H);
+
     return 0;
 }
